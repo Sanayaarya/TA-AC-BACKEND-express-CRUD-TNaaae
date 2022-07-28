0ff543a2df6e73556545031366e4479ef4fee69b
Sanaya Arya <sanayaarya0123@gmail.com>
2022-07-28 13:52:20 +0530

    completed

--- block-BNaacv/user-dairy/router/user.js
+++ block-BNaacv/user-dairy/router/user.js
@@ -32,49 +32,4 @@ router.delete('/:id',(req,res)=>{
   // delete that user
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// router.get('/', (req,res) => {
-//     let list = ["Aman" ,"Ritesh" , "Abhay" , "Ranvijay"]
-//     res.render('users.ejs',{list : list})
-// })
-// router.get('/new', (req,res) => {
-//     res.render('userForm.ejs')
-// })
-
-// router.get('/:id', (req,res) => {
-//     res.render('singleUser.ejs', {name : "Aman" , age : 22})
-// })
-
-
-
-// router.post('/new', (req,res) => {
-//     res.send(req.body)
-//     console.log(req.body)
-// })
-
-// router.get("/:id/edit" , (req,res)=>{
-
-// })
-// router.put("/:id" , (req, res)=>{
-    
-// })
-
 module.exports = router
